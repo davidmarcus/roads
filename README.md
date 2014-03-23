382daeecf5701395fe242db77f79a0d8f794b030
David Marcus <davidmarcus@davidmarcus-laptop.(none)>
2014-03-23 01:49:49 -0700

    Reorganize methods into a RoadGraph class.

--- README.md
+++ README.md
@@ -16,34 +16,35 @@ Code is currently contained entirely within roads.py. It's easiest to interact w
 ###Create a graph for a 4x4 grid
 ```python
 from roads import *
-G = grid_graph(4, 4)
+
+G = RoadGraph.grid_graph(4, 4)
 ```
 ###Visualize the graph
 ```python
-show(G)
+G.show()
 ```
 ###Calculate key metrics for the graph
 ```python
-print 'graph_length=%s' % graph_length(G)
-print 'mean_path_length=%s' % mean_path_length(G)
+print 'graph_length=%s' % G.length
+print 'mean_path_length=%s' % G.mean_path_length
 ```
 ###Drop two edges from the graph
 ```python
-edge = get_removal_target(G)
+edge = G.get_removal_target()
 G.remove_edge(*edge)
-edge = get_removal_target(G)
+edge = G.get_removal_target()
 G.remove_edge(*edge)
 show(G)
 ```
 ###Add an edge to the graph
 ```python
-edge = get_addition_target(G)
-add_edge(G, edge)
+edge = G.get_addition_target()
+G.add_edge(*edge)
 show(G)
 ```
 ###Add or remove edges, depending on whether there is capacity for a new edge without exceeding the target graph_length
 ```python
 for i in xrange(10): # run 10 iterations of adding or removing edges
-  iterate(G)
+  G.iterate()
 show(G)
 ```
--- roads.py
+++ roads.py
@@ -4,77 +4,96 @@ import random
 import math
 import numpy
 
-X = Y = 4
-def graph_length(G):
-    """The length of all roads in a graph"""
-    return sum(data['weight'] for node0, node1, data in G.edges_iter(data=True))
+X = Y = 4 # default dimensions for the road graph
 
-def make_weighted(G):
-    """The length of all roads in a graph"""
-    for edge in G.edges():
-        G.remove_edge(*edge)
-        G.add_edge(*edge, weight=math.sqrt((edge[0][0] - edge[1][0])**2 + (edge[0][1] - edge[1][1])**2))
+class RoadGraph(nx.Graph):
 
-def grid_graph(x, y):
-    """A graph representing a rectangular street grid with width x and height y"""
-    G = nx.grid_2d_graph(x, y)
-    make_weighted(G)
-    return G
+    # construction
+    @classmethod
+    def from_graph(cls, graph):
+        """Create a RoadGraph instance of any graph of type nx.Graph"""
+        G = cls()
+        G.add_edges_from(graph.edges())
+        G.make_weighted()
+        return G
 
-def mean_path_length(G):
-    """The mean path length between nodes in the graph"""
-    return numpy.mean([nx.dijkstra_path_length(G, node0, node1) for node0 in G.nodes() for node1 in G.nodes() if isinstance(node0[0], int) and isinstance(node1[0], int)])
+    def make_weighted(self):
+        """Weight every edge of the graph by calculating the length of each edge"""
+        for edge in self.edges():
+            self.remove_edge(*edge)
+            self.add_edge(*edge)
+            
+    @classmethod
+    def grid_graph(cls, x, y):
+        """A graph representing a rectangular street grid with width x and height y"""
+        G = nx.grid_2d_graph(x, y)
+        return cls.from_graph(G)
 
-def show(G):
-    """Draw the graph"""
-    nx.draw_networkx(G, pos={n: n for n in G.nodes_iter()}, with_labels=False, node_size=20)
-    plt.show()
+    # visualization
+    def show(self):
+        """Draw the graph"""
+        nx.draw_networkx(self, pos={n: n for n in self.nodes_iter()}, with_labels=False, node_size=20)
+        plt.show()
 
-def get_removal_target(G):
-    """Find an edge to drop"""
-    results = []
-    for node0, node1, data in G.edges(data=True):
-        edge = node0, node1
-        G.remove_edge(*edge)
-        if nx.is_connected(G):
-            results.append((mean_path_length(G), edge))
-        G.add_edge(*edge, weight=data['weight'])
-    weight, edge = sorted(results)[0]
-    print 'DROPPING %s, mean_path_length: %s' % (edge, weight)
-    return edge
+    # metrics
+    @property
+    def length(self):
+        """The length of all roads in a graph"""
+        return sum(data['weight'] for node0, node1, data in self.edges_iter(data=True))
 
-def get_addition_target(G):
-    """Find an edge to add"""
-    results = []
-    for i in xrange(1, X):
-        for j in xrange(Y):
-            node = (i, j)
-            for edge in [(node, (i - 1, j - 1)), (node, (i - 1, j)), (node, (i - 1, j + 1))]:
-                if 0 <= edge[-1][-1] < Y and not G.has_edge(*edge):
-                    G_copy = G.copy()
-                    add_edge(G_copy, edge)
-                    results.append((mean_path_length(G_copy), edge))
-    weight, edge = sorted(results)[0]
-    print 'ADDING %s, mean_path_length=%s' % (edge, weight)
-    return edge
-    
-def add_edge(G, edge):
-    """Add an edge to the graph G"""
-    ((x0, y0), (x1, y1)) = edge
-    if x0 == x1 or y0 == y1 or not G.has_edge((x0, y1), (x1, y0)):
-        G.add_edge(*edge, weight=math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2))
-    else:
-        G.remove_edge((x0, y1), (x1, y0))
-        midpoint = (numpy.mean([x0, x1]), numpy.mean([y0, y1]))
-        for x in [x0, x1]:
-            for y in [y0, y1]:
-                G.add_edge(midpoint, (x, y), weight=math.sqrt(2.0) / 2)
+    @property
+    def mean_path_length(self):
+        """The mean path length between nodes in the graph"""
+        return numpy.mean([nx.dijkstra_path_length(self, node0, node1) for node0 in self.nodes() for node1 in self.nodes() if isinstance(node0[0], int) and isinstance(node1[0], int)])
+        
+    # optimization
+    def get_removal_target(self):
+        """Find an edge to drop"""
+        results = []
+        for node0, node1, data in self.edges(data=True):
+            edge = node0, node1
+            self.remove_edge(*edge)
+            if nx.is_connected(self):
+                results.append((self.mean_path_length, edge))
+            self.add_edge(*edge)
+        weight, edge = sorted(results)[0]
+        print 'DROPPING %s, mean_path_length: %s' % (edge, weight)
+        return edge
 
-def iterate(G, target=graph_length(grid_graph(X, Y))):
-    """Add or remove an edge, based on whether the graph has capacity to add an edge and stay within the target length"""
-    if graph_length(G) <= target - math.sqrt(2):
-        edge = get_addition_target(G)
-        add_edge(G, edge)
-    else:
-        edge = get_removal_target(G)
-        G.remove_edge(*edge)
+    def get_addition_target(self):
+        """Find an edge to add"""
+        results = []
+        for i in xrange(1, X):
+            for j in xrange(Y):
+                node = (i, j)
+                for edge in [(node, (i - 1, j - 1)), (node, (i - 1, j)), (node, (i - 1, j + 1))]:
+                    if 0 <= edge[-1][-1] < Y and not self.has_edge(*edge):
+                        G_copy = self.__class__.from_graph(self)
+                        G_copy.add_edge(*edge)
+                        results.append((G_copy.mean_path_length, edge))
+        weight, edge = sorted(results)[0]
+        print 'ADDING %s, mean_path_length=%s' % (edge, weight)
+        return edge
+
+    def add_edge(self, node0, node1):
+        """Add an edge to the graph G"""
+        (x0, y0), (x1, y1) = node0, node1
+        if x0 == x1 or y0 == y1 or not self.has_edge((x0, y1), (x1, y0)):
+            super(RoadGraph, self).add_edge(node0, node1, weight=math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2))
+        else:
+            self.remove_edge((x0, y1), (x1, y0))
+            midpoint = (numpy.mean([x0, x1]), numpy.mean([y0, y1]))
+            for x in [x0, x1]:
+                for y in [y0, y1]:
+                    super(RoadGraph, self).add_edge(midpoint, (x, y), weight=math.sqrt(2.0) / 2)
+
+    def iterate(self, target=(X * (Y - 1) + Y * (X - 1))):
+        """Add or remove an edge, based on whether the graph has capacity to add an edge and stay within the target length"""
+        if self.length <= target - math.sqrt(2):
+            edge = self.get_addition_target()
+            self.add_edge(*edge)
+        else:
+            edge = self.get_removal_target()
+            self.remove_edge(*edge)
+
+
